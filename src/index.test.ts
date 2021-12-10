f3d33891c384a3a31dc35b40b5d2bbd8465b978b
Marco Cotrufo <marco.cotrufo@devncode.it>
2021-12-10 15:36:28 +0100

    style(eslint): use proper eslint configuration

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,57 +1,65 @@
-import eslint from "./index"
+import { Linter } from "eslint";
+import eslint from "./index";
 
-declare const global: any
+declare const global: any;
 
 const mockFileContents = (contents: string) => {
-  const asyncContents: Promise<string> = new Promise((resolve, reject) =>
-    resolve(contents),
-  )
-  return async (path: string): Promise<string> => asyncContents
-}
+  const asyncContents: Promise<string> = new Promise((resolve) => {
+    resolve(contents);
+  });
+  return async (): Promise<string> => asyncContents;
+};
 
-const defaultConfig = {
+const defaultConfig: Linter.Config = {
   extends: "eslint:recommended",
-}
+};
+const override: Linter.Config = {
+  rules: {
+    "prettier/prettier": 0,
+  },
+};
 
 describe("eslint()", () => {
   beforeEach(() => {
-    global.warn = jest.fn()
-    global.message = jest.fn()
-    global.fail = jest.fn()
-    global.markdown = jest.fn()
-  })
+    global.warn = jest.fn();
+    global.message = jest.fn();
+    global.fail = jest.fn();
+    global.markdown = jest.fn();
+  });
 
   afterEach(() => {
-    global.warn = undefined
-    global.message = undefined
-    global.fail = undefined
-    global.markdown = undefined
-  })
+    global.warn = undefined;
+    global.message = undefined;
+    global.fail = undefined;
+    global.markdown = undefined;
+  });
 
   it("does not lint anything when no files in PR", async () => {
     global.danger = {
       github: { pr: { title: "Test" } },
       git: { created_files: [], modified_files: [] },
-    }
+    };
 
-    await eslint(defaultConfig)
+    await eslint(defaultConfig, override);
 
-    expect(global.fail).not.toHaveBeenCalled()
-  })
+    expect(global.fail).not.toHaveBeenCalled();
+  });
 
   it("does not fail when a valid file is in PR", async () => {
     global.danger = {
       github: {
         pr: { title: "Test" },
-        utils: { fileContents: mockFileContents(`1 + 1;`) },
+        utils: {
+          fileContents: mockFileContents('console.log("hello world");\n'),
+        },
       },
       git: { created_files: ["foo.js"], modified_files: [] },
-    }
+    };
 
-    await eslint(defaultConfig)
+    await eslint(defaultConfig, override);
 
-    expect(global.fail).not.toHaveBeenCalled()
-  })
+    expect(global.fail).not.toHaveBeenCalled();
+  });
 
   it("calls fail for each eslint violation", async () => {
     global.danger = {
@@ -59,23 +67,20 @@ describe("eslint()", () => {
         pr: { title: "Test" },
         utils: {
           fileContents: mockFileContents(
-            `
-          var foo = 1 + 1;
-          console.log(foo);
-        `.trim(),
+            'var foo = 1 + 1;console.log("foo");\n'
           ),
         },
       },
       git: { created_files: ["foo.js"], modified_files: [] },
-    }
+    };
 
-    await eslint(defaultConfig)
+    await eslint(defaultConfig, override);
 
-    expect(global.fail).toHaveBeenCalledTimes(1)
+    expect(global.fail).toHaveBeenCalledTimes(2);
     expect(global.fail).toHaveBeenLastCalledWith(
-      "foo.js line 2 – 'console' is not defined. (no-undef)",
-    )
-  })
+      "foo.js line 1 – 'foo' is assigned a value but never used. (no-unused-vars)"
+    );
+  });
 
   it("uses the provided eslint config", async () => {
     global.danger = {
@@ -83,20 +88,23 @@ describe("eslint()", () => {
         pr: { title: "Test" },
         utils: {
           fileContents: mockFileContents(`
-          var foo = 1 + 1;
+          const foo = 1 + 1;
           console.log(foo);
         `),
         },
       },
       git: { created_files: ["foo.js"], modified_files: [] },
-    }
+    };
 
-    await eslint({
-      rules: {
-        "no-undef": 0,
+    await eslint(
+      {
+        rules: {
+          "no-undef": 0,
+        },
       },
-    })
+      override
+    );
 
-    expect(global.fail).not.toHaveBeenCalled()
-  })
-})
+    expect(global.fail).not.toHaveBeenCalled();
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,42 @@
-// Provides dev-time type structures for  `danger` - doesn't affect runtime.
-import { DangerDSLType } from "../node_modules/danger/distribution/dsl/DangerDSL"
-declare var danger: DangerDSLType
-export declare function message(message: string): void
-export declare function warn(message: string): void
-export declare function fail(message: string): void
-export declare function markdown(message: string): void
+import { ESLint, Linter } from "eslint";
+import { DangerDSLType } from "../node_modules/danger/distribution/dsl/DangerDSL";
 
-import { ESLint, Linter } from "eslint"
+/* eslint-disable */
+declare const danger: DangerDSLType
+export declare function message(): void
+export declare function warn(): void
+export declare function fail(message: string): void
+export declare function markdown(): void
+/* eslint-enable */
+
+async function lintFile(linter: ESLint, path: string) {
+  const contents = await danger.github.utils.fileContents(path);
+  const results = await linter.lintText(contents, { filePath: path });
+  const formatter = await linter.loadFormatter("json");
+  const resultText: Linter.FixReport[] = JSON.parse(formatter.format(results));
+
+  resultText[0].messages.map((msg) => {
+    if (msg.fatal) {
+      fail(`Fatal error linting ${path} with eslint.`);
+      return;
+    }
+
+    const fn = { 1: warn, 2: fail }[msg.severity];
+
+    fn(`${path} line ${msg.line} – ${msg.message} (${msg.ruleId})`);
+  });
+}
 
 /**
  * Eslint your code with Danger
  */
-export default async function eslint(config: any) {
+export default async function eslint(
+  config: Linter.Config,
+  override?: Linter.Config
+) {
   const filesToLint = danger.git.created_files.concat(
-    danger.git.modified_files,
-  )
-  const cli = new ESLint({ baseConfig: config })
-  return Promise.all(filesToLint.map((f) => lintFile(cli, config, f)))
-}
-
-async function lintFile(linter: ESLint, config: Linter.Config, path: string) {
-  const contents = await danger.github.utils.fileContents(path)
-  const results = await linter.lintText(contents, { filePath: path })
-  const formatter = await linter.loadFormatter("json")
-  const resultText: Linter.FixReport[] = JSON.parse(formatter.format(results))
-
-  resultText[0].messages.map((msg) => {
-    if (msg.fatal) {
-      fail(`Fatal error linting ${path} with eslint.`)
-      return
-    }
-
-    const fn = { 1: warn, 2: fail }[msg.severity]
-
-    fn(`${path} line ${msg.line} – ${msg.message} (${msg.ruleId})`)
-  })
+    danger.git.modified_files
+  );
+  const cli = new ESLint({ baseConfig: config, overrideConfig: override });
+  return Promise.all(filesToLint.map((f) => lintFile(cli, f)));
 }
